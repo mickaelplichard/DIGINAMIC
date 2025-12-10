3f4d68637615b33a73565ffa034dd572c44a6b46
Mickael Plichard <mickael.plichard@gmail.com>
2025-12-10 17:20:13 +0100

    Update tp03

--- SQL/tp03.sql
+++ SQL/tp03.sql
@@ -8,10 +8,10 @@ SELECT * FROM article WHERE prix >= 2 AND prix <= 6.25;
 
 SELECT * FROM article WHERE prix BETWEEN 2 AND 6.25;
  
-SELECT a.* FROM article a JOIN fournisseur f ON a.id_fou = f.id WHERE (a.prix < 2 OR a.prix > 6.25) AND f.nom = 'Française d''Imports' ORDER BY a.prix DESC;
+SELECT article.* FROM article JOIN fournisseur ON article.id_fou = fournisseur.id WHERE (article.prix < 2 OR article.prix > 6.25) AND fournisseur.nom = 'Française d''Imports' ORDER BY article.prix DESC;
 
-SELECT a.* FROM article a JOIN fournisseur f ON a.id_fou = f.id WHERE f.nom IN ('Française d''Imports', 'Dubois & Fils');
+SELECT article.* FROM article JOIN fournisseur ON article.id_fou = fournisseur.id WHERE fournisseur.nom IN ('Française d''Imports', 'Dubois & Fils');
 
-SELECT a.* FROM article a JOIN fournisseur f ON a.id_fou = f.id WHERE f.nom NOT IN ('Française d''Imports', 'Dubois & Fils');
+SELECT article.* FROM article JOIN fournisseur ON article.id_fou = fournisseur.id WHERE fournisseur.nom NOT IN ('Française d''Imports', 'Dubois & Fils');
 
 SELECT * FROM bon WHERE date_cmde BETWEEN '2019-02-01' AND '2019-04-30';
