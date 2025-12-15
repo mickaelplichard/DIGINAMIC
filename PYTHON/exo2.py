444d0ab61fbd94c585411a7900b087b9a1c46fdf
Mickael Plichard <mickael.plichard@gmail.com>
2025-12-15 11:45:17 +0100

    exo2

---
+++ PYTHON/exo2.py
@@ -0,0 +1,12 @@
+x = 42
+print(id(x))
+
+
+a: int = 66
+b = a
+print(id(a), id(b), sep="\n")
+
+
+message1, message 2 = "Première chaînes de caractères", "Deuxième chaînes de caractères"
+message2, message1 = message1, message2
+print(message1, message2, sep="\n")
