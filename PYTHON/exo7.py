a23e3223202ad96a8dc048a8ab6ebcddf947de4a
Mickael Plichard <mickael.plichard@gmail.com>
2025-12-15 12:45:58 +0100

    exo7

---
+++ PYTHON/exo7.py
@@ -0,0 +1,12 @@
+
+nom = input("Quel est votre nom: ")
+prenom = input("Quel est votre prénom: ")
+age = input("Quel est votre âge: ")
+print (f"{nom}, {prenom}, {age}")
+
+
+
+nombre_entier = input("Choisissez un nombre entier: ")
+nombre_entier: int = int(nombre_entier)
+nombre_entier = nombre_entier + 3.14
+print(nombre_entier)
