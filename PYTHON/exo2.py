1f806a4c7120f95643d6f2119d960b47a8b9d23f
Mickael Plichard <mickael.plichard@gmail.com>
2025-12-15 11:47:23 +0100

    exo2

--- PYTHON/exo2.py
+++ PYTHON/exo2.py
@@ -7,6 +7,6 @@ b = a
 print(id(a), id(b), sep="\n")
 
 
-message1, message 2 = "Première chaînes de caractères", "Deuxième chaînes de caractères"
+message1, message2 = "Première chaînes de caractères", "Deuxième chaînes de caractères"
 message2, message1 = message1, message2
 print(message1, message2, sep="\n")
