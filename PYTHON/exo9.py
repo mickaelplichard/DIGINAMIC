539121750053d03e352ca910328093697ca572c8
Mickael Plichard <mickael.plichard@gmail.com>
2025-12-15 15:32:50 +0100

    exo9

---
+++ PYTHON/exo9.py
@@ -0,0 +1,68 @@
+# EXO 1 ----------------
+result = sum([15, 23])
+print(result)
+
+result = 42 - 57
+print(result)
+
+result = 6 * 9
+print(result)
+
+result = 100 / 25
+print(result)
+
+result = 29 % 5
+print(result)
+
+result = 29 // 5
+print(result)
+
+result = 2 ** 10
+print(result)
+
+
+
+# EXO 2 --------------
+import math
+
+result = math.sqrt(81); print(result)
+result = math.floor(7.8); print(result)
+result = math.ceil(7.2); print(result)
+
+
+
+# EXO 3 -----------------
+a = 10; a += 5
+b = 20; b -= 7
+c = 3; c *= 4
+d = 50; d /= 2
+e = 9; e %= 4
+print(a, b, c, d, e, sep="\n")
+
+
+
+# EXO 4 --------------------
+result = 15 > 10; print(result)
+result = 8 <= 8; print(result)
+result = 20 == 25; print(result)
+result = 30 != 30; print(result)
+result = "Bonjour" == "bonjour"; print(result)
+
+
+
+# EXO 5 -------------------------
+string = "programmation"
+result = "a" in string
+print(result)
+
+sentence = "Le Python est un langage génial"
+result = "Python" in sentence
+print(result)
+
+string = "éducation"
+result = "z" not in string
+print(result)
+
+list = [2, 4, 6, 8]
+result = 10 not in list
+print(result)
