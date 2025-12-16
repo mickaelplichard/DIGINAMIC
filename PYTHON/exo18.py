479876fca3d6b0a40fd9d21858c3d9deef473555
Mickael Plichard <mickael.plichard@gmail.com>
2025-12-16 15:52:39 +0100

    exo18

---
+++ PYTHON/exo18.py
@@ -0,0 +1,6 @@
+set_1 = {"coucou", True, 2, 't', 76, False, "test"}
+set_2 = {"python", 2.2, 'x', False, "test", 76, "la réponse D"}
+
+union = set_1 | set_2; print(union)
+intersect = set_1 & set_2; print(intersect)
+diff = set_1 ^ set_2; print(diff)
