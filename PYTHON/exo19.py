687541fd51a2b9c4383100972d0fb30829a9d183
Mickael Plichard <mickael.plichard@gmail.com>
2025-12-17 14:33:11 +0100

    exo19

---
+++ PYTHON/exo19.py
@@ -0,0 +1,18 @@
+
+def extendator()
+    pass
+
+
+
+
+def add(a, b):
+    return a + b
+
+def carre(a):
+    return a ** 2
+
+result = add(carre(2), carre(3))
+
+print(add(2,3))
+print(carre(2))
+print (result)
